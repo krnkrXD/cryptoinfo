05787b373618a0ef3f087c8fac6b703f57749040
Karan <124873800+krnkrXD@users.noreply.github.com>
2023-06-22 19:32:15 +0530

    Update App.js - change the route path of home page from / to cryptoinfo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
                 <Layout>
                     <div className="routes">
                     <Routes>
-                        <Route path="/home" element={<Homepage />} />
+                        <Route path="cryptoinfo" element={<Homepage />} />
                         <Route path="cryptocurrencies" element={<Cryptocurrencies />} />
                         <Route path="crypto/:coinId" element={<CryptoDetails />} /> 
                         <Route path="news" element={<News />} />    

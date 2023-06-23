29ef424594533758b36c336ed6e10408da7d9b0f
krnkrxd <karankumarbarolia@gmail.com>
2023-06-23 10:42:22 +0530

    Change the routes in index, homepage in package and title of web page

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ const App = () => {
                     <div className="routes">
                     <Routes>
                         <Route path="/" element={<Homepage />} />
-                        <Route path="cryptocurrencies" element={<Cryptocurrencies />} />
-                        <Route path="crypto/:coinId" element={<CryptoDetails />} /> 
-                        <Route path="news" element={<News />} />    
+                        <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                        <Route path="/crypto/:coinId" element={<CryptoDetails />} /> 
+                        <Route path="/news" element={<News />} />    
                     </Routes>
                     </div>
                 </Layout>
